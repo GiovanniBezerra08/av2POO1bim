db2f200ddcfe7b8be98d681cff907039d9974455
GiovanniBezerra08 <giovannibezerra3@gmail.com>
2025-06-16 10:18:37 -0300

    1606

--- avl1-1bim/Assets/Scripts/Ninja.cs
+++ avl1-1bim/Assets/Scripts/Ninja.cs
@@ -1,11 +1,33 @@
 using UnityEngine;
 
-public class Templario : Personagem
+public class Ninja : Personagem
     
 {
     public enum ArmaDoNinja
     {
         SHURINKEN, CLONE
     }
-    
+
+    private ArmaDoNinja poderes;
+
+    public void setAmaDoNinja(ArmaDoNinja poderes);
+    {
+        this.poderes = poderes;
+    }
+
+    public ArmaDoNinja getArmaDoNinja()
+    {
+        return poderes;
+    }
+
+    public int DanoDoPersonagem()
+    {
+        int buff = 0;
+
+        switch (poderes)
+        {
+            case ArmaDoNinja.SHURINKEN:
+                buff = getForcaAtaque() + 40;
+        }
+    }
 }
---
+++ avl1-1bim/Assets/Scripts/Teste.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Teste : MonoBehaviour
+{
+    
+    Ninja ninja = new Ninja();
+    Personagem samurai = new Personagem();
+ 
+    void Start()
+    {
+        ninja.setAgilidade();
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+}
